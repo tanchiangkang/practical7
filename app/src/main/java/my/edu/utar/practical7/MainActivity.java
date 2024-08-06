259df8b7648286507c154e09c8689d14af435688
tanchiangkang <86589335+tanchiangkang@users.noreply.github.com>
2024-08-06 14:54:27 +0800

    Optional dynamic buffer for reading all content

--- app/src/main/java/my/edu/utar/practical7/MainActivity.java
+++ app/src/main/java/my/edu/utar/practical7/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -30,18 +32,44 @@ public class MainActivity extends AppCompatActivity {
         new Thread() {
             public void run() {
 
+//                try {
+//                    URL url = new URL("https://tools.ietf.org/html/rfc2616");
+//                    HttpURLConnection hc = (HttpURLConnection) url.openConnection();
+//                    final String result;
+//
+//                    int length = hc.getContentLength();
+//
+//                    InputStream input = url.openStream();
+//                    //Fixed buffer size of 1KBytes
+//                    byte[] bInput = new byte[1000];
+//                    int readSize = input.read(bInput);
+//                    result = new String(bInput);
+//
+//                    handler.post(new Runnable() {
+//
+//                        @Override
+//                        public void run() {
+//                            tv.setText(result);
+//                        }
+//                    });
+//                    //tv.setText(result);
+//
+//                    input.close();
+//                } catch (IOException e) {
+//                    Log.e("Net", "Error", e);
+//                }
+
+                //Optional
+                //Use BufferedInputStream to more efficiently read content
+                //over the network. getContentLength() does not always return
+                //actual content length of a page. It is however okay to use
+                //when accessing web services.
                 try {
-                    URL url = new URL("https://tools.ietf.org/html/rfc2616");
+                    URL url = new URL("https://tools.ietf.org/rfc/rfc2616.txt");
                     HttpURLConnection hc = (HttpURLConnection) url.openConnection();
                     final String result;
-
-                    int length = hc.getContentLength();
-
-                    InputStream input = url.openStream();
-                    //Fixed buffer size of 1KBytes
-                    byte[] bInput = new byte[1000];
-                    int readSize = input.read(bInput);
-                    result = new String(bInput);
+                    InputStream in = new BufferedInputStream(hc.getInputStream());
+                    result = readStream(in);
 
                     handler.post(new Runnable() {
 
@@ -50,14 +78,25 @@ public class MainActivity extends AppCompatActivity {
                             tv.setText(result);
                         }
                     });
-                    //tv.setText(result);
-
-                    input.close();
-                } catch (IOException e) {
+                } catch (Exception e) {
                     Log.e("Net", "Error", e);
                 }
             }
         }.start();
+    }
 
+    //Optional
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while (i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
     }
 }
